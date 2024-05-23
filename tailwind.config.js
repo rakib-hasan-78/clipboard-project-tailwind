32bafd62ee930943228cf68a664454638c173074
rakib-hasan-78 <rakib.webdev78@gmail.com>
2024-05-23 18:20:22 +0600

    clipboard first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens:{
+      sm:'480px',
+      md:'768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    extend: {
+      fontFamily:{
+        sans:['Bai Jamjuree', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
